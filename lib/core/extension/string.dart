476990576bdf77b072c3b17420e931322a2b7c09
CodeAlchemist09 <dipankargalaxy1020@gmail.com>
2026-09-11 17:45:45 +0530

    Enhance isValidAPIKey to trim input string

--- lib/core/extension/string.dart
+++ lib/core/extension/string.dart
@@ -1,7 +1,8 @@
 extension StringExtension on String {
   bool isValidAPIKey() {
+    final trimmed = trim();
     return RegExp(
-      r'^AIzaSy[A-Za-z0-9_-]{33}$',
-    ).hasMatch(this);
+      r'^(AIzaSy[A-Za-z0-9_-]{33}|AQ\.[A-Za-z0-9_\.-]{30,})$',
+    ).hasMatch(trimmed);
   }
 }
